8f27ec3ca7d5e5e44c3663aefd227bec3ff09be5
augusting52 <augusting1991@163.com>
2017-07-13 07:49:57 +0800

    Author : libing use okhttp to request github_api, and print response with log

--- okhttpdemo/app/src/main/java/com/rzfsc/okhttpdemo/MainActivity.java
+++ okhttpdemo/app/src/main/java/com/rzfsc/okhttpdemo/MainActivity.java
@@ -5,11 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final String GITHUB_API = "https://api.github.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +37,52 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                new Thread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                doOkHttpRequest();
+                            }
+                        }
+                ).start();
+
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
+    }
+
+    private void doOkHttpRequest() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(GITHUB_API)
+                .build();
+
+        try {
+
+            try (Response response = okHttpClient.newCall(request).execute()) {
+                String responseText = response.body().string();
+                responseText = responseText.replace("{", "[").replace("}", "]");
+                responseText = "{" + responseText.substring(1, responseText.length() - 1) + "}";
+                Log.d(TAG, "doOkHttpRequest: response = " + responseText);
+                JSONObject jsonObject = new JSONObject(responseText);
+                Iterator keys = jsonObject.keys();
+                while (keys.hasNext()) {
+                    String key = (String) keys.next();
+                    if (jsonObject.has(key)) {
+                        String value = jsonObject.optString(key);
+                        value = value.replace("[", "{").replace("]", "}");
+                        Log.d(TAG, "response key = [" + key +  "], value = [" + value + "]");
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
